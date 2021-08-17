ccda14d9187aa606556867d01a24ff1621e1ffb2
Julian_A_Wardana <julian.wardana@binus.ac.id>
2021-08-17 20:05:38 +0700

    make middleware

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/form-article', 'ArticleController@index')->name('form-article');
-Route::post('/form-article', 'ArticleController@store')->name('get-article');
-Route::get('/article', 'ArticleController@show')->name('list-article');
-Route::get('/view-article/{id}', 'ArticleController@detail')->name('view-article');
-Route::get('/edit-article/{id}', 'ArticleController@edit')->name('edit-article');
-Route::put('/update-article/{id}', 'ArticleController@update')->name('update-article');
-Route::delete('/delete-article/{id}', 'ArticleController@delete')->name('delete-article');
-Route::get('/my-article', 'ArticleController@myArticle')->name('my-article');
-Route::post('/article/{id}', 'CommentController@store')->name('comment-article');
+Route::get('/form-article', 'ArticleController@index')->name('form-article')->middleware('auth');;
+Route::post('/form-article', 'ArticleController@store')->name('get-article')->middleware('auth');;
+Route::get('/article', 'ArticleController@show')->name('list-article')->middleware('auth');;
+Route::get('/view-article/{id}', 'ArticleController@detail')->name('view-article')->middleware('auth');;
+Route::get('/edit-article/{id}', 'ArticleController@edit')->name('edit-article')->middleware('auth');;
+Route::put('/update-article/{id}', 'ArticleController@update')->name('update-article')->middleware('auth');;
+Route::delete('/delete-article/{id}', 'ArticleController@delete')->name('delete-article')->middleware('auth');;
+Route::get('/my-article', 'ArticleController@myArticle')->name('my-article')->middleware('auth');;
+Route::post('/article/{id}', 'CommentController@store')->name('comment-article')->middleware('auth');;
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
